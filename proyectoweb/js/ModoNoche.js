39a7c5a3cfee83eb7f4fea37ac117ba11baa5458
Manuel4242 <74304914+Manuel4242@users.noreply.github.com>
2021-05-19 14:19:26 +0200

    Modo noche añadido

---
+++ proyectoweb/js/ModoNoche.js
@@ -0,0 +1,29 @@
+function modoNoche(){
+    $('#cabecera').css('background', 'black');
+    $('#cabecera-dolly').css('background', 'black');
+    $('#subnav').css('background', 'black');
+    $('#subnav-art').css('background', 'black');
+    $('#myLog').css('background', 'black');
+    $('body').css('background', 'black');
+    $('.fila').css('background', 'black');
+    $('*').css('color', 'white');
+    $('#user').css('color', 'grey');
+    $('#contraseña').css('color', 'grey');
+    $('i').css('color', 'grey');
+    $('select').css('color', 'grey');
+    $('textarea').css('color', 'grey');
+    $('input').css('border', 'black');
+    $('input').css('background', '#f2f2f2');
+    $('input').css('color', 'grey');
+    $('.acordeon').css('background', 'black');
+    $('.panel').css('background', 'black');
+    $('.content h2').css('color', 'black');
+    $('.content p').css('color', 'black');
+    
+    $('#noche').css('display', 'none');
+    $('#dia').css('display', 'block');
+}
+
+function modoDia() {
+    location.reload();
+}
